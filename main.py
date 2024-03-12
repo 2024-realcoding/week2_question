802fa55d2107fccc9bc7e79e534832e7bc18d56b
dohyeon321 <127070191+dohyeon321@users.noreply.github.com>
2024-03-12 22:11:02 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from priority_queue import PriorityQueue
 def main():
   pq = PriorityQueue()
-  
+
   pq.push("Task 1", 3)
   pq.push("Task 2", 1)
   pq.push("Task 3", 2)
-  
+
   print(pq.pop())
   print(pq.pop())
   print(pq.pop())
