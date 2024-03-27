e64fa190687a517dc1cbf392b16d98619446219b
chaeyeon <111040042+yeonicy@users.noreply.github.com>
2024-03-27 11:11:00 +0900

    Update priority_queue.py

--- priority_queue.py
+++ priority_queue.py
@@ -35,11 +35,11 @@ class PriorityQueue:
       smallest = index
 
       if left_child_index < len(self.heap) and \
-        self.heap[left_child_index][0] > self.heap[smallest][0]:
+        self.heap[left_child_index][0] < self.heap[smallest][0]:
         smallest = left_child_index
 
       if right_child_index < len(self.heap) and \
-        self.heap[right_child_index][0] > self.heap[smallest][0]:
+        self.heap[right_child_index][0] < self.heap[smallest][0]:
         smallest = right_child_index
 
       if smallest != index:
